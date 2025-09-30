6cae5de4ccbac2ebfd1caf6d83b84717a758f81a
nrkn <narakorn458@gmail.com>
2025-10-01 03:12:35 +0700

    feat(item): add item-list page

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -17,8 +17,6 @@ export function NavigationBar() {
       className="border-b border-[1px] border-[rgba(0,0,0,0.045)] py-[1px] sm:px-[10px] md:px-[45px] px-[25px]"
     >
       <NavbarBrand className="!flex-none mr-3">
-        {" "}
-        {/* Optional spacing tweak */}
         <Link href="/" className="font-bold text-xl flex items-center">
           <Image
             src="/images/cnclogo.png"
@@ -33,7 +31,7 @@ export function NavigationBar() {
       <NavbarContent justify="start" className="flex gap-5 items-center">
         <NavbarItem>
           <Link
-            href="/"
+            href="/items"
             className="text-foreground hover:text-primary transition hover:text-[rgba(27,160,240,1)]"
           >
             ยืม-คืนสิ่งของ
@@ -65,7 +63,7 @@ export function NavigationBar() {
         </NavbarItem>
       </NavbarContent>
 
-      {/* Right Content */}
+      {/* TODO: นำ user token มาเช็ก หากมี user token (already logged in) ให้โชว์แค่ profile หากไม่มีให้โชว์ปุ่ม login ไม่โชว์ทั้งคู่พร้อมกัน */}
       <NavbarContent justify="end">
         <NavbarItem>
           <div className="flex items-center">
@@ -76,6 +74,17 @@ export function NavigationBar() {
               เข้าสู่ระบบ
             </Link>
           </div>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/" className="flex items-center">
+            <Image
+              src="/images/to-be-added-later" 
+              alt="User Avatar"
+              width={60}
+              height={60}
+              className="rounded-full border-2 border-gray-300 hover:border-primary transition"
+            />
+          </Link>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Search } from "lucide-react";
+
+export default function SearchBar() {
+  return (
+    <div className="flex justify-center w-full mt-5">
+      <div className="flex flex-col items-center w-full max-w gap-4">
+        <div className="relative w-full flex items-center justify-center">
+          <Link
+            href="/items"
+            className="absolute left-0 h-12 px-4 rounded-full bg-[rgb(255,225,106)] border-black border flex items-center justify-center text-[rgb(1,51,82)] font-[400] text-[16px] hover:scale-90 hover:bg-black hover:text-white transition-all"
+          >
+            การยืมของฉัน
+          </Link>
+          <div className="relative w-1/2 max-w">
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full h-12 pl-10 pr-4 border rounded-full text-start"
+            />
+          </div>
+        </div>
+
+        
+        <button className="h-10 px-6 w-45 rounded-full bg-[rgb(0,163,247)] border-black border text-white font-[400] text-[16px] hover:scale-90 hover:bg-black transition-all flex items-center justify-center">
+          ค้นหา
+        </button>
+      </div>
+    </div>
+  );
+}
