af748c7a39339d059ec52d052d5afa89efa6d071
nrkn <narakorn458@gmail.com>
2025-10-04 16:58:41 +0700

    feat(borrow-return): add my-borrow page and allow passing a custom component as a prop to SearchBar

--- src/app/borrow-return/my-borrow/page.tsx
+++ src/app/borrow-return/my-borrow/page.tsx
@@ -1,24 +1,16 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import MovingCloudBG from "../../components/MovingCloudBG";
-import ItemCard from "../../components/ItemCard";
+import MovingCloudBG from "@/src/components/MovingCloudBG";
+import ItemCard from "@/src/components/ItemCard";
 import SearchBar from "@/src/components/SearchBar";
+import { Undo2 } from "lucide-react";
 
 export default function Home() {
   const data: { id: string; status: string; amount: number }[] = [
-    { id: "Nvidia RTX5090", status: "Available", amount: 10 },
-    { id: "Router", status: "Available", amount: 7 },
-    { id: "Raspberry Pi", status: "Disappeared", amount: 0 },
-    { id: "RJ45", status: "In use - Due 2025/10/11", amount: 0 },
-    { id: "360 Camera", status: "Available", amount: 1 },
-    {
-      id: "Fundmentals of Artificial Intelligence Books",
-      status: "Available",
-      amount: 13,
-    },
-    { id: "Mac Mini", status: "In use - Due 2025/10/08", amount: 0 },
-    { id: "USB Hub", status: "Disappeared", amount: 0 },
+    { id: "Router", status: "Due 2025/10/11", amount: 7 },
+    { id: "RJ45", status: "Due 2025/10/11", amount: 0 },
+    { id: "Mac Mini", status: "Due 2025/10/08", amount: 0 },
   ];
   return (
     <main className="flex flex-col justify-start items-center gap-20 mt-5 pt-5">
@@ -28,15 +20,18 @@ export default function Home() {
           <div className="flex flex-col justify-start justify-items-center sm:justify-items-start items-center sm:items-start text-center gap-5 ">
             <h3 className="text-3xl sm:text-3xl md:text-4xl font-bold">
               <span className="">ระบบยืม-คืนสิ่งของ </span>
+              <br></br>
+              <span className="text-2xl font-semibold">การยืมของฉัน </span>
             </h3>
           </div>
 
           <SearchBar>
             <Link
-              href="/borrow-return/my-borrow"
-              className="absolute left-0 h-12 px-4 rounded-full bg-[rgb(255,225,106)] border-black border flex items-center justify-center text-[rgb(1,51,82)] font-[400] text-[16px] hover:scale-90 hover:bg-black hover:text-white transition-all"
+              href="/borrow-return"
+              className="absolute left-0 h-12 px-4 gap-2 rounded-full bg-[rgb(255,225,106)] border-black border flex items-center justify-center text-[rgb(1,51,82)] font-[400] text-[16px] hover:scale-90 hover:bg-black hover:text-white transition-all"
             >
-              การยืมของฉัน
+              <Undo2></Undo2>
+              ถอยหลัง
             </Link>
           </SearchBar>
 
