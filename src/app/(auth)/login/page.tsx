788bdc85c03bd093c7b448c4521f5a0c21a1c68f
PerTrakanjan <thanakrit.tra@ku.th>
2025-10-05 18:27:07 +0700

    style: format code

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,114 +1,134 @@
 "use client";
 
-import CustomInput from "@/src/components/CustomInput";
-import {Button, Link} from "@heroui/react";
+import CustomInput from "@/src/components/CustomInput"
+import { Button, Link } from "@heroui/react"
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { apiClient } from "@/src/services/apiClient";
+import { useEffect, useState } from "react"
+import { redirect, useRouter, useSearchParams } from "next/navigation"
+import { apiClient } from "@/src/services/apiClient"
+import { useAuthService } from "@/src/services/authService"
 
-export default function LoginPage(){
-    const router = useRouter();
-    const [msg, setMsg] = useState("");
-    const [loading, setLoading] = useState(false);
+export default function LoginPage() {
+  const searchParams = useSearchParams()
+  const code = searchParams.get("code")
 
-    async function onLocalSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        setLoading(true);
-        setMsg("");
+  const router = useRouter()
+  const [msg, setMsg] = useState("")
+  const [loading, setLoading] = useState(false)
 
-        const form = new FormData(e.currentTarget);
-        const email = String(form.get("email"));
-        const password = String(form.get("password"));
+  const { googleLogin } = useAuthService()
 
-        // ⭐ Debug: ดูว่าส่งอะไรไป 
-        console.log("📤 Sending login request:", { email, password});
+  async function googleLoginHandler() {
+    const url = await googleLogin()
+    redirect(url.data)
+  }
 
-        try {
-            const res = await apiClient.post("/auth/login", {email, password});
-            
-            // ⭐ Debug: ดู response ที่ได้กลับมา
-            console.log("📥 Response status:", res.status);
-            console.log("📥 Response data:", res.data);
+  async function onLocalSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setLoading(true)
+    setMsg("")
 
-            if (res.status === 200){
-                console.log("📥 Login successful:");
+    const form = new FormData(e.currentTarget)
+    const email = String(form.get("email"))
+    const password = String(form.get("password"))
 
-                // 📦 เก็บไว้ใน localStorage
-                const { access_token, refresh_token } = res.data;
-                localStorage.setItem("access_token", access_token);
-                localStorage.setItem("refresh_token", refresh_token);
-                console.log("🎉 Tokens saved to localStorage");
-                console.log("Access Token:", access_token);
-                router.replace("/")
+    // ⭐ Debug: ดูว่าส่งอะไรไป
+    console.log("📤 Sending login request:", { email, password })
 
-            } else {
-                const errorMsg = typeof res.data?.error === "string" ? res.data.error : `error ${res.status}`;
-                console.log("⚠️ Non-200 status:", errorMsg);
-                
-                setMsg(errorMsg)
-            }
-        } catch (err: any){
-            // ⭐ Debug: ดู error ที่เกิดขึ้น
-            console.log("❌ Login error occurred");
-            console.log("Error status:", err?.response?.status);
-            console.log("Error data:", err?.response?.data);
-            console.log("Full error:", err);
+    try {
+      const res = await apiClient.post("/v1/auth/login", { email, password })
 
-            const errorMsg = err?.response?.data?.error ?? "login failed";
-            setMsg(errorMsg);
-        } finally {
-            setLoading(false);
-        }
+      // ⭐ Debug: ดู response ที่ได้กลับมา
+      console.log("📥 Response status:", res.status)
+      console.log("📥 Response data:", res.data)
+
+      if (res.status === 200) {
+        console.log("📥 Login successful:")
+
+        // 📦 เก็บไว้ใน localStorage
+        const { access_token, refresh_token } = res.data
+        localStorage.setItem("access_token", access_token)
+        localStorage.setItem("refresh_token", refresh_token)
+        console.log("🎉 Tokens saved to localStorage")
+        console.log("Access Token:", access_token)
+        router.replace("/")
+      } else {
+        const errorMsg =
+          typeof res.data?.error === "string"
+            ? res.data.error
+            : `error ${res.status}`
+        console.log("⚠️ Non-200 status:", errorMsg)
+
+        setMsg(errorMsg)
+      }
+    } catch (err: any) {
+      // ⭐ Debug: ดู error ที่เกิดขึ้น
+      console.log("❌ Login error occurred")
+      console.log("Error status:", err?.response?.status)
+      console.log("Error data:", err?.response?.data)
+      console.log("Full error:", err)
+
+      const errorMsg = err?.response?.data?.error ?? "login failed"
+      setMsg(errorMsg)
+    } finally {
+      setLoading(false)
     }
+  }
 
-    return (
-        <div className="p-6 bg-background rounded-xl shadow-lg w-[400px]">
-            <h1 className="text-3xl font-semibold mb-5 text-center">Login</h1>
-            <div className="text-center">
-                <Button name="google-login" type="submit" className="w-4/5 mx-auto mb-2 bg-neutral-second ">
-                    <div className="w-5 h-5 bg-white rounded-full flex items-center justify-center">
-                        <img
-                            src="/icons/google-icon.svg"
-                            className="w-5 h-5"
-                        />
-                    </div>
-                    Continue with Google
-                </Button>
-                <p className="text-2xl text-center">Or</p>
-            </div>
-            <form onSubmit={onLocalSubmit} className="w-full max-w-md space-y-4">
-                <CustomInput
-                    name="email"
-                    label="Email"
-                    type="email"
-                    placeholder="you@gmail.com"
-                    svgSrc="/icons/mail.svg"
-                    isRequired
-                />
-                <CustomInput
-                    name="password"
-                    label="Password"
-                    type="password"
-                    placeholder="********"
-                    svgSrc="/icons/password.svg"
-                    isRequired
-                />
+  return (
+    <div className="p-6 bg-background rounded-xl shadow-lg w-[400px]">
+      <h1 className="text-3xl font-semibold mb-5 text-center">Login</h1>
+      <div className="text-center">
+        <Button
+          name="google-login"
+          type="submit"
+          className="w-4/5 mx-auto mb-2 bg-neutral-second"
+          onPress={googleLoginHandler}
+        >
+          <div className="w-5 h-5 bg-white rounded-full flex items-center justify-center">
+            <img src="/icons/google-icon.svg" className="w-5 h-5" />
+          </div>
+          Continue with Google
+        </Button>
+        <p className="text-2xl text-center">Or</p>
+      </div>
+      <form onSubmit={onLocalSubmit} className="w-full max-w-md space-y-4">
+        <CustomInput
+          name="email"
+          label="Email"
+          type="email"
+          placeholder="you@gmail.com"
+          svgSrc="/icons/mail.svg"
+          isRequired
+        />
+        <CustomInput
+          name="password"
+          label="Password"
+          type="password"
+          placeholder="********"
+          svgSrc="/icons/password.svg"
+          isRequired
+        />
 
-                {msg && <p className="text-center text-red-600 text-sm">{msg}</p>}
+        {msg && <p className="text-center text-red-600 text-sm">{msg}</p>}
 
-                <div className="text-center">
-                    <Button color="primary" type="submit" className="mt-5 w-1/2" isLoading={loading}>
-                        Login
-                    </Button>
-                    <p className="pt-2">Don't have an account&nbsp;
-                    <Link href="/register" size="sm" underline="always">
-                    Register
-                    </Link>
-                    </p>
-                </div>
-                
-            </form>
+        <div className="text-center">
+          <Button
+            color="primary"
+            type="submit"
+            className="mt-5 w-1/2"
+            isLoading={loading}
+          >
+            Login
+          </Button>
+          <p className="pt-2">
+            Don't have an account&nbsp;
+            <Link href="/register" size="sm" underline="always">
+              Register
+            </Link>
+          </p>
         </div>
-      );
+      </form>
+    </div>
+  )
 }
---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react"
+import { useRouter } from "next/navigation"
+import { User } from "../types"
+import { ACCESS_TOKEN } from "../constants/token"
+
+type AuthContextType = {
+  user: User | null
+  token: string | null
+  logout: () => void
+  isAuthenticated: boolean
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null)
+  const [token, setToken] = useState<string | null>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem(ACCESS_TOKEN)
+    // const storedUser = localStorage.getItem("user")
+    if (storedToken) {
+      setToken(storedToken)
+      // setUser(parse(storedUser))
+    }
+  }, [])
+
+  const logout = () => {
+    setToken(null)
+    setUser(null)
+    localStorage.removeItem(ACCESS_TOKEN)
+    localStorage.removeItem("user")
+    router.push("/login")
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        token,
+        logout,
+        isAuthenticated: !!token,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  const ctx = useContext(AuthContext)
+  if (!ctx) throw new Error("useAuth must be used within an AuthProvider")
+  return ctx
+}
