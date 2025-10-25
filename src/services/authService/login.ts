ee23864e9750e012465d912a242c774cb3368ea3
PerTrakanjan <thanakrit.tra@ku.th>
2025-10-25 23:37:12 +0700

    fix(auth): correct user role handling and improve login response structure

--- src/services/authService/login.ts
+++ src/services/authService/login.ts
@@ -8,18 +8,15 @@ export const login = async ({
     password: string;
 }) => {
     try {
-        const res = await apiClient.post("v1/auth/login", { email, password });
+        const res = await apiClient.post<{ access_token: string, refresh_token: string}>("v1/auth/login", { email, password });
 
-        const { access_token, refresh_token } = res.data;
-        localStorage.setItem("access_token", access_token);
-        localStorage.setItem("refresh_token", refresh_token);
-
-        return { success: true, status: res.status };
+        return { success: true, status: res.status, data: res.data };
     } catch (err: any) {
         return {
             success: false,
             status: err?.response?.status || 500,
             error: err?.response?.data?.error || "Login failed",
+            data: null
         };
     }
 };
